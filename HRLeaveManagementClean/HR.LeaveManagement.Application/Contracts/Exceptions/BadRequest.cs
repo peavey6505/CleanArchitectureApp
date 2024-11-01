9575f466f0098e9269aa1186a386371ff15898d9
Tomasz Lenart <tomaszlenart96@gmail.com>
2024-11-01 01:33:24 +0100

    Added missing Exceptions

---
+++ HRLeaveManagementClean/HR.LeaveManagement.Application/Contracts/Exceptions/BadRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace HR.LeaveManagement.Application.Contracts.Exceptions
+{
+    public class BadRequest : Exception
+    {
+        public BadRequest(string message) : base(message)
+        {
+            
+        }
+    }
+}
