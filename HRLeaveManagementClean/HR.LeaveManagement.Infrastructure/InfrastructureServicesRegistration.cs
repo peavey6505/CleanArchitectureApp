8aab667acb4becf433e476d952077da419da6fd6
Tomasz Lenart <tomaszlenart96@gmail.com>
2024-11-02 12:08:51 +0100

    Added email service

---
+++ HRLeaveManagementClean/HR.LeaveManagement.Infrastructure/InfrastructureServicesRegistration.cs
@@ -0,0 +1,20 @@
+﻿using HR.LeaveManagement.Application.Contracts.Email;
+using HR.LeaveManagement.Application.Models.Email;
+using HR.LeaveManagement.Infrastructure.EmailService;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HR.LeaveManagement.Infrastructure
+{
+    public static class InfrastructureServicesRegistration
+    {
+        public static IServiceCollection ConfigureInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<EmailSettings>(options => configuration.GetSection("EmailSettings"));
+            services.AddTransient<IEmailSender, IEmailSender>();
+
+            return services;
+        }
+
+    }
+}
